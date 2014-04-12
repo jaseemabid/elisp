f150451dcc69dda3cad54c254d0b52c4dcb55e4d
Satish Srinivasan <satish@ideadevice.com>
2014-04-13 02:25:08 +0530

    Interface to youtrack api in elisp

---
+++ youtrack.el
@@ -0,0 +1,68 @@
+;; please have the following variables setup:
+;; (setq yt-user "username")
+;; (setq yt-password "password")
+;; (setq yt-baseurl "http://youtrack-server:port")
+;; then just call yt-bug to report a bug
+
+(require 'url)
+
+(defun http-post (url args)
+  "Send ARGS to URL as a POST request."
+
+  (let ((url-request-method "POST")
+	(url-request-extra-headers
+	 '(("Content-Type" . "application/x-www-form-urlencoded")))
+	(url-request-data
+	 (mapconcat (lambda (arg)
+		      (concat (url-hexify-string (car arg))
+			      "="
+			      (url-hexify-string (cdr arg))))
+		    args
+                        "&")))
+
+    (url-retrieve url 'dump-url-buffer)))
+
+(defun http-put (url args)
+  "Send ARGS to URL as a PUT request."
+
+  (setq args (mapconcat (lambda (arg)
+	       (concat (url-hexify-string (car arg))
+		       "="
+		       (url-hexify-string (cdr arg))))
+	     args
+	     "&"))
+
+  (print (format "PUT %s" (concat url "?" args)))
+  (let ((url-request-method "PUT")
+	(url-request-extra-headers '(("Content-Length" . "0"))))
+    (url-retrieve (concat url "?" args) 'dump-url-buffer)))
+
+(defun dump-url-buffer (status)
+  "The buffer contains the raw HTTP response sent by the server."
+  (switch-to-buffer (current-buffer))) ; use kill-buffer if you don't want to see response
+
+(defun yt-bug (project summary &optional description)
+  "creates a youtrack issue"
+
+  (interactive "sProj. Shortname: \nsSummary: \nsDesc: ")
+
+  (defun login (baseurl user password)
+    "authenticates with youtrack"
+    (let
+	((url-path "/rest/user/login"))
+      (http-post (format "%s%s" baseurl url-path)
+		 (list `("login" . ,user) `("password" . ,password)))))
+
+  (if (eq description nil)
+      (setq description ""))
+
+  (let
+      ((url-path "/rest/issue"))
+
+    (login yt-baseurl yt-user yt-password)
+    (http-put (concat yt-baseurl url-path)
+		      (list `("project" . ,project) ; shortname of the project
+			    `("summary" . ,summary)
+			    `("description" . ,description)))))
+
+(provide 'youtrack)
