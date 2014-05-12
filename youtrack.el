90284b8faee5616ecc4886f9af5186d5a32719bb
Jaseem Abid <jaseemabid@gmail.com>
2014-05-12 14:47:58 +0530

    Release under MIT license

--- youtrack.el
+++ youtrack.el
@@ -1,7 +1,33 @@
 ;;; youtrack.el --- Youtrack mode for emacs
 
-;; Author: Jaseem <j@ideadevice.com>, Satish Srinivasan <satish@ideadevice.com>
+;; Author: Jaseem Abid <jaseemabid@gmail.com>
+;;         Satish Srinivasan <satish@ideadevice.com>
+;; Created: Sun Apr 13 02:25:08 2014 +0530
 ;; Keywords: youtrack, bug db, issue tracker
+
+;; This file is not part of GNU Emacs.
+
+;; The MIT License (MIT)
+
+;; Copyright (C) 2014 Jaseem Abid <jaseemabid@gmail.com>
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Install:
 
